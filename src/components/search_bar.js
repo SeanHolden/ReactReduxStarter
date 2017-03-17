0cf382aecb680f781be9215dc13645c7c7e31abb
Sean Holden <seanholden1@gmail.com>
2017-03-17 17:01:51 +0000

    Can interact with SearchBar component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,4 +1,22 @@
-const SearchBar = () => {
-  return <input />;
+import React, { Component } from 'react';
 
+class SearchBar extends Component {
+  constructor(props) {
+    // super: call parent method (in Component)
+    super(props);
+
+    // whenever we do a setState, the component re-renders in the DOM
+    this.state = { term: '' };
+  }
+
+  render() {
+    return(
+      <div>
+        <input onChange={(event) => this.setState({ term: event.target.value })} />
+        Value of the input: { this.state.term }
+      </div>
+    );
+  }
 }
+
+export default SearchBar;
