5b7f8078169059458051f6867beaf9753582c5b9
Sean Holden <seanholden1@gmail.com>
2017-03-24 17:11:39 +0000

    added styling and searchbar functionality

--- src/index.js
+++ src/index.js
@@ -16,7 +16,11 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -27,8 +31,8 @@ class App extends Component {
   render() {
     return(
       <div>
-        <SearchBar />
-        <VideoDetail video={this.state.selectedVideo} />
+        <SearchBar onSearchTermChange={ term => this.videoSearch(term) } />
+        <VideoDetail video={ this.state.selectedVideo } />
         <VideoList
           onVideoSelect={ (selectedVideo) => this.setState({ selectedVideo }) }
           videos={ this.state.videos }
